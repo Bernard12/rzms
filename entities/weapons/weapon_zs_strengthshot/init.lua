71f6b0fe84a476ac1322bb3b509b110f8502d494
Bernard12 <ivnsimakhin@gmail.com>
2020-06-26 15:19:32 +0300

    Initial commit

---
+++ entities/weapons/weapon_zs_strengthshot/init.lua
@@ -0,0 +1,11 @@
+INC_SERVER()
+
+SWEP.Primary.Projectile = "projectile_strengthdart"
+SWEP.Primary.ProjVelocity = 2000
+
+a = 15
+
+function SWEP:EntModify(ent)
+	ent:SetSeeked(self:GetSeekedPlayer() or nil)
+	ent.BuffDuration = self.BuffDuration
+end
