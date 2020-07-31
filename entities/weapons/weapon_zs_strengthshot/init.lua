04aecc40de89931ed3b6f1e883de29f3e8e4628d
Bernard12 <ivnsimakhin@gmail.com>
2020-07-31 11:12:09 +0300

    New color and refactor

--- entities/weapons/weapon_zs_strengthshot/init.lua
+++ entities/weapons/weapon_zs_strengthshot/init.lua
@@ -3,8 +3,6 @@ INC_SERVER()
 SWEP.Primary.Projectile = "projectile_strengthdart"
 SWEP.Primary.ProjVelocity = 2000
 
-a = 15
-
 function SWEP:EntModify(ent)
 	ent:SetSeeked(self:GetSeekedPlayer() or nil)
 	ent.BuffDuration = self.BuffDuration
